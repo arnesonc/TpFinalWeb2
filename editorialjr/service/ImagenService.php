5a4dd5aa660b3ae785e6c62659d81b701d4f4bb0
lucasnogueira955 <lucasnogueira955@outlook.com>
2016-05-31 23:19:12 -0300

    Se agrega el service para imagenModel, imagenService

--- editorialjr/service/ImagenService.php
+++ editorialjr/service/ImagenService.php
@@ -11,8 +11,60 @@ class ImagenService{
 		$this->dataAccess = new DataAccess;
 	}
 	
-	public function getImagenById($ImagenId){
+	public function getImagenById($idImagen){
+		$sql = " SELECT id, id_articulo, url  FROM imagen WHERE id = $idImagen;";
+
+		try{
+		
+			$imagenDB = $this->dataAccess->getOneResult($sql);
+		
+		}catch(Exception $e){
+				
+			throw new Exception($e);
+		
+		}
+		return $this->convertImagenDBToImagenModel($imagenDB);
+	}
+
+	
+	private function convertImagenDBToImagenModel($imagenDB){
+	
+		/* Convierto el resultado de la BD a un objeto modelado */
+		$imagen = new ImagenModel;
+		$imagen->id = $imagenDB["id"];
+		$imagen->idArticulo =$imagenDB["id_articulo"];
+		$imagen->url = $imagenDB["url"];
+		return $imagen;
 		
 	}
+	
+	public function getAllImagesByIdArticulo($idArticulo){
+		
+		$sql = " SELECT id, url, id_articulo FROM imagen I  WHERE I.id_articulo = $idArticulo;";
+	
+		try{
+				
+			$imagenDBArray = $this->dataAccess->getMultipleResults($sql);
+	
+		}catch(Exception $e){
+				
+			throw new Exception($mensaje);
+	
+		}
+	
+	
+		$arrayImagenModel = array();
+	
+		foreach ($imagenDBArray as $imagenDB) {
+	
+			$imagenModel = $this->convertImagenDBToImagenModel($imagenDB);
+				
+			$arrayImagenModel[] = $imagenModel;
+		}
+	
+		return $arrayImagenModel;
+	}
+	
 }
+
 ?>
