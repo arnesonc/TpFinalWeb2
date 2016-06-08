bef7e1d0decff1ebe9e9b3fd2e84e125330f0ef4
lucasnogueira955 <lucasnogueira955@outlook.com>
2016-06-07 21:15:41 -0300

    Se crea PaisAjaxHelper

--- editorialjr/index.php
+++ editorialjr/index.php
@@ -1,37 +1,36 @@
 <?php
-
-require_once(__DIR__."/service/UsuarioService.php");
-require_once(__DIR__."/service/CiudadService.php");
-require_once(__DIR__."/service/RolService.php");
-require_once(__DIR__."/service/RegionService.php");
-require_once(__DIR__."/service/PublicacionService.php");
-require_once(__DIR__."/service/NumeroService.php");
-require_once(__DIR__."/service/ImagenService.php");
-require_once(__DIR__."/service/EstadoArticuloService.php");
-require_once(__DIR__."/service/EstadoNumeroService.php");
-require_once(__DIR__."/model/UsuarioModel.php");
-require_once(__DIR__."/helpers/ValidationHelper.php");
-require_once(__DIR__."/common/Dates.php");
+require_once (__DIR__ . "/service/UsuarioService.php");
+require_once (__DIR__ . "/service/CiudadService.php");
+require_once (__DIR__ . "/service/RolService.php");
+require_once (__DIR__ . "/service/RegionService.php");
+require_once (__DIR__ . "/service/PublicacionService.php");
+require_once (__DIR__ . "/service/NumeroService.php");
+require_once (__DIR__ . "/service/ImagenService.php");
+require_once (__DIR__ . "/service/EstadoArticuloService.php");
+require_once (__DIR__ . "/service/EstadoNumeroService.php");
+require_once (__DIR__ . "/model/UsuarioModel.php");
+require_once (__DIR__ . "/helpers/ValidationHelper.php");
+require_once (__DIR__ . "/common/Dates.php");
 
 /* Usar siempre solo este require para logger */
-require_once(__DIR__."/helpers/LoggerHelper.php");
+require_once (__DIR__ . "/helpers/LoggerHelper.php");
 
-$service = new PublicacionService;
+$service = new PublicacionService ();
 
-var_dump($service->getPublicacionById(1));
+var_dump ( $service->getPublicacionById ( 1 ) );
 
-$usuarioService = new UsuarioService;
+$usuarioService = new UsuarioService ();
 
-if(false){
-	$usuarioModel = new UsuarioModel;
+if (false) {
+	$usuarioModel = new UsuarioModel ();
 	$usuarioModel->email = "redactor@redactor.com";
 	$usuarioModel->pass = "1234";
 	$usuarioModel->nombre = "redactor";
 	$usuarioModel->apellido = "jr";
 	
-	try{
-		$creado = $usuarioService->createUsuario($usuarioModel);
-	}catch(Exception $e){
+	try {
+		$creado = $usuarioService->createUsuario ( $usuarioModel );
+	} catch ( Exception $e ) {
 		echo $e;
 	}
 	echo "creado: $creado";
@@ -39,16 +38,16 @@ if(false){
 
 // agrega publicacion y numero corrobora los getter.
 
-if(false){
-	$publicacionModel = new PublicacionModel;
+if (false) {
+	$publicacionModel = new PublicacionModel ();
 	$publicacionModel->id_usuario = 1;
 	$publicacionModel->nombre = "publicacionfalsa";
 	$publicacionModel->destacado = "false";
-	//FIXME: corroborar si se debe invocar al metodo
-	$publicacionModel->getFechaUltimoNumero();
-	$publicacionModel->url_ultima_portada="asd.edu.ar";
+	// FIXME: corroborar si se debe invocar al metodo
+	$publicacionModel->getFechaUltimoNumero ();
+	$publicacionModel->url_ultima_portada = "asd.edu.ar";
 	
-	$numeroModel = new NumeroModel;
+	$numeroModel = new NumeroModel ();
 	$numeroModel->id = null;
 	$numeroModel->id_publicacion = "null";
 	$numeroModel->id_estado_numero = 1;
@@ -57,49 +56,49 @@ if(false){
 	$numeroModel->precio = 120;
 	$numeroModel->fecha_publicado = "null";
 	
-	$publicacionService = new PublicacionService;
-	//echo "".$publicacionModel->nombre;
-	try{
-		$creado = $publicacionService->createPublicacionNumero($publicacionModel,$numeroModel);
-	} catch (Exception $e){
-		echo "error".$e;
+	$publicacionService = new PublicacionService ();
+	// echo "".$publicacionModel->nombre;
+	try {
+		$creado = $publicacionService->createPublicacionNumero ( $publicacionModel, $numeroModel );
+	} catch ( Exception $e ) {
+		echo "error" . $e;
 	}
 	echo "creado: $creado";
 	
-	$numeroService = new NumeroService;
-	$arrayNumeroModel = $numeroService->getAllNumeros(14);
+	$numeroService = new NumeroService ();
+	$arrayNumeroModel = $numeroService->getAllNumeros ( 14 );
 	
-	echo "ultima fecha publicada: ".$publicacionModel->getFechaUltimoNumero();
+	echo "ultima fecha publicada: " . $publicacionModel->getFechaUltimoNumero ();
 }
 ?>
 
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Tp Final</title>
-	</head>
-	<body>
-		<script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
-		<script type="text/javascript" src="js/editorialjr.js"></script>
-		<div>
-			<p>
+<head>
+<title>Tp Final</title>
+</head>
+<body>
+	<script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
+	<script type="text/javascript" src="js/editorialjr.js"></script>
+	<div>
+		<p>
 				Usuario:
 				<?php //echo $usuario->email . " Rol: " . $usuario->getRol()->descripcion; ?>
 			</p>
-		</div>
-		<div>
+	</div>
+	<div>
 		<button id="btnObtenerUsuarioAdmin">Obtener usuario admin</button>
 		<span id="spnUsuarioAdmin"></span>
-		</div>
-		<div>
-			<button id="btnTest">Test Ajax</button>
-			<span id="spnTestAjax"></span>
-		</div>
-		<div>
-			<button id="btnObtenerCiudadesBsAs">Obtener ciudades de Bs As</button>
-			<div id="resultadoCiudades"></div>
-		</div>
+	</div>
+	<div>
+		<button id="btnTest">Test Ajax</button>
+		<span id="spnTestAjax"></span>
+	</div>
+	<div>
+		<button id="btnObtenerCiudadesBsAs">Obtener ciudades de Bs As</button>
+		<div id="resultadoCiudades"></div>
+	</div>
 
-	</body>
+</body>
 </html>
