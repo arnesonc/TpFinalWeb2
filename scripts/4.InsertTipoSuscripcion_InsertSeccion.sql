6649a0a83c9c287a0a43b8120835d541346cb9c8
arnesonc <leo123456>
2016-06-10 09:20:39 -0300

    InsertTipoSuscripcion_InsertSeccion

---
+++ scripts/4.InsertTipoSuscripcion_InsertSeccion.sql
@@ -0,0 +1,48 @@
+
+use editorialjr;
+
+/* completo los tipos de suscripcion */
+INSERT INTO tipo_suscripcion (id,cantidad_meses)
+VALUES 
+(null,'1'),
+(null,'3'),
+(null,'6'),
+(null,'12');
+
+/* borro y vuelvo a crear la tabla seccion / completo secciones base */
+DROP TABLE seccion;
+
+create table seccion(
+id int primary key auto_increment not null,
+nombre varchar(50) not null
+);
+
+INSERT INTO seccion (id,nombre)
+VALUES 
+(null, 'Información general'),
+(null, 'Carta de lectores'),
+(null, 'Celebridades'),
+(null, 'Ciencia'),
+(null, 'Cultura'),
+(null, 'Deporte'),
+(null, 'Economía'),
+(null, 'Editorial'),
+(null, 'Educación'),
+(null, 'Empresa'),
+(null, 'Especiales'),
+(null, 'Espectáculo'),
+(null, 'Internacionales'),
+(null, 'Moda'),
+(null, 'Música'),
+(null, 'Opinión'),
+(null, 'Policiales'),
+(null, 'Política'),
+(null, 'Religión'),
+(null, 'Reportajes'),
+(null, 'Rural'),
+(null, 'Salud'),
+(null, 'Servicios'),
+(null, 'Sociedad'),
+(null, 'Tecnología'),
+(null, 'Televisión'),
+(null, 'Turismo');
