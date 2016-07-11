f469f429a3082a9634b0ef10986e792e3d162aaa
lucasnogueira955 <lucasnogueira955@outlook.com>
2016-07-11 18:08:53 -0300

    mejora en la vista: se agrega consistencia de heade

--- editorialjr/index.php
+++ editorialjr/index.php
@@ -17,12 +17,14 @@ require_once(__DIR__."/common/sesionValidaIndex.php");
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/logo-nav.css" rel="stylesheet">
-
     <!-- Nuestro CSS -->
     <link href="css/style.css" rel="stylesheet">
 
+    <!-- Side Bar CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+    <!-- CSS de las Datatables-->
+    <link rel="stylesheet" type="text/css" href="css/datatables.min.css" />
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -34,56 +36,49 @@ require_once(__DIR__."/common/sesionValidaIndex.php");
 
 <body>
 
-<!-- Navigation -->
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">
-                <img src="img/logo.png" alt="Logo Editorial Jr">
-            </a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <?php
-                    if(isset($_SESSION['session']) && isset($_SESSION['session']["login"])
-                    && $_SESSION['session']["login"] == "ok"){
-                        ?>
-                        <li>
-                            <a>Bienvenido <?php echo $_SESSION['session']['nombre']; ?>! </a>
-                        </li>
-                        <li>
-                            <a href="/logoutIndex.php">Cerrar sesión</a>
-                        </li>
-                        <li>
-                            <a href="/client-listar-suscripciones.php">mis suscripciones</a>
-                        </li>
-                <?php }else{ ?>
-
-                        <li>
-                            <a id="btnIniciarSesion" href="#">Iniciar sesión</a>
-                        </li>
-                        <li>
-                            <a href="RegistrarCliente.php">Registrarse</a>
-                        </li>
-                <?php } ?>
-                <!-- <li>
-                    <a href="#">contacto</a>
-                </li> -->
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-</nav>
+  <!-- Navigation -->
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="/index.php">Editorial Jr</a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                  <li>
+                      <a href="#">About</a>
+                  </li>
+                  <li>
+                      <a href="#">Services</a>
+                  </li>
+                  <?php
+                      if(isset($_SESSION['session']) && isset($_SESSION['session']["login"])
+                      && $_SESSION['session']["login"] == "ok"){
+                  ?>
+                          <li>
+                              <a href="/logoutIndex.php">Cerrar sesión</a>
+                          </li>
+                  <?php }else{ ?>
+                          <li>
+                              <a id="btnIniciarSesion" href="#">Iniciar sesión</a>
+                          </li>
+                          <li>
+                              <a href="RegistrarCliente.php">Registrarse</a>
+                          </li>
+                  <?php } ?>
+              </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container -->
+  </nav>
 
 <section id="section_ultimos">
     <div class="container">
