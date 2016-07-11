6661b009dca4fa7457ed49e0bc73dc5737607b3d
lucasnogueira955 <lucasnogueira955@outlook.com>
2016-07-11 19:09:26 -0300

    pequeño arreglo en la vista

--- editorialjr/js/ClientListarNumeros.js
+++ editorialjr/js/ClientListarNumeros.js
@@ -11,7 +11,7 @@ function listarNumeros(id_publicacion) {
 				type : 'POST',
 				dataType : "json",
 				success : function(result) {
-					armarTablaNumeros(result);
+					armarTablaNumeros(result,false);
 				},
 				error : function(error) {
 					alert("Ups, ocurrio un error! " + error);
@@ -29,7 +29,7 @@ function listarNumerosComprados() {
 				type : 'POST',
 				dataType : "json",
 				success : function(result) {
-					armarTablaNumeros(result);
+					armarTablaNumeros(result,true);
 				},
 				error : function(error) {
 					alert("Ups, ocurrio un error! " + error);
@@ -37,18 +37,20 @@ function listarNumerosComprados() {
 		});
 }
 
-function armarTablaNumeros(listaNumeros){
+function armarTablaNumeros(listaNumeros,campoFechaDeCompra){
 
 	  var tabla = "";
 	  $("#bodyNumeros").html("");
 
 	  tabla = "<table id='tblNumeros' class='table table-striped table-bordered table-responsive' cellspacing='0'>";
-	  tabla +="<thead><tr><th>Publicacion</th><th>Numero</th><th>publicado el dia</th><th>fecha compra</th>";
-	  tabla +="<th>Acciones</th></tr></thead><tbody>";
+	  tabla +="<thead><tr><th>Publicacion</th><th>Numero</th><th>publicado el dia</th>";
+		if(campoFechaDeCompra){tabla += "<th>fecha compra</th>";}
+		tabla +="<th>Acciones</th></tr></thead><tbody>";
 
 	  $.each(listaNumeros, function(index, numero) {
 		tabla += "<tr><td>" + numero.nombre_publicacion + "</td><td>" + numero.numero_revista + "</td>";
-	  tabla += "<td>" + numero.fecha_publicado + "</td><td>" + numero.fecha_de_compra + "</td>";
+	  tabla += "<td>" + numero.fecha_publicado + "</td>";
+		if(campoFechaDeCompra){tabla += "<td>" + numero.fecha_de_compra + "</td>";}
 		tabla += "<td><button onclick='RedirectArticulos("+ numero.id +","+ numero.id_estado_numero +");' id='btnListarArticulos' name='" + numero.id + "' class='btn btn-success'><span class='glyphicon glyphicon-list'></span> Ver Articulos</button>  ";
 		tabla += "<button onclick='RedirectArticulos("+ numero.id +","+ numero.id_estado_numero +");' id='btnListarArticulos' name='" + numero.id + "' class='btn btn-warning'><span class='glyphicon glyphicon-list'></span> Descargar en PDF</button>  ";
 		});
