b6e3167c3264409220aa37e9ddb9f7b1d8435acf
lucasnogueira955 <lucasnogueira955@outlook.com>
2016-07-11 18:50:08 -0300

    Se agregan las dependencias de client-listar-numeros.php Completamente funcional

--- editorialjr/js/ClientListarNumeros.js
+++ editorialjr/js/ClientListarNumeros.js
@@ -19,18 +19,36 @@ function listarNumeros(id_publicacion) {
 		});
 }
 
+function listarNumerosComprados() {
+			$.ajax({
+				url : '/helpers/NumeroAjaxHelper.php',
+				data : {
+					metodo : "listarNumerosComprados",
+					idCliente : obtenerSessionID()
+				},
+				type : 'POST',
+				dataType : "json",
+				success : function(result) {
+					armarTablaNumeros(result);
+				},
+				error : function(error) {
+					alert("Ups, ocurrio un error! " + error);
+				}
+		});
+}
+
 function armarTablaNumeros(listaNumeros){
 
 	  var tabla = "";
 	  $("#bodyNumeros").html("");
 
 	  tabla = "<table id='tblNumeros' class='table table-striped table-bordered table-responsive' cellspacing='0'>";
-	  tabla +="<thead><tr><th>Numero</th><th>fecha publicado</th>";
+	  tabla +="<thead><tr><th>Publicacion</th><th>Numero</th><th>fecha publicado</th>";
 	  tabla +="<th>Acciones</th></tr></thead><tbody>";
 
 	  $.each(listaNumeros, function(index, numero) {
 		fecha=(numero.fecha_publicado == null) ? '<strong>DRAFT</strong>' : numero.fecha_publicado;
-		tabla += "<tr><td>" + numero.numero_revista + "</td>";
+		tabla += "<tr><td>" + numero.nombre_publicacion + "</td><td>" + numero.numero_revista + "</td>";
 	    tabla += "<td>" + fecha + "</td>";
 		tabla += "<td><button onclick='RedirectArticulos("+ numero.id +","+ numero.id_estado_numero +");' id='btnListarArticulos' name='" + numero.id + "' class='btn btn-success'><span class='glyphicon glyphicon-list'></span> Ver Articulos</button>  ";
 		tabla += "<button onclick='RedirectArticulos("+ numero.id +","+ numero.id_estado_numero +");' id='btnListarArticulos' name='" + numero.id + "' class='btn btn-warning'><span class='glyphicon glyphicon-list'></span> Descargar en PDF</button>  ";
--- editorialjr/model/NumeroModel.php
+++ editorialjr/model/NumeroModel.php
@@ -14,61 +14,62 @@ class NumeroModel{
 	public $precio;
 	public $fecha_publicado;
 	public $numero_revista;
-	
+	public $nombre_publicacion;
+
 	private $estado_numero;
 	private $publicacion;
-	
+
 	/**
 	 * Obtiene un objeto EstadoNumero, si lo tiene en memoria lo devuelve, si no, lo va a buscar a la base de datos
 	 * */
 	public function getEstadoNumero(){
-	
+
 		if(is_null($this->estado_numero)){
 			$estadoNumeroService = new EstadoNumeroService;
-				
+
 			try {
-	
+
 				$this->estado_Numero = $estadoNumeroService->getEstadoNumeroById($this->id_estado_numero);
-	
+
 			}catch(Exeption $e){
 				$logger = Logger::getRootLogger();
 				$logger->error($e);
 				return null;
 			}
 		}
-	
+
 		return $this->estado_numero;
 	}
-	
+
 	/**
 	 * Obtiene un objeto Publicacion, si lo tiene en memoria lo devuelve, si no, lo va a buscar a la base de datos
 	 * */
 	public function getPublicacion(){
-	
+
 		if(is_null($this->publicacion)){
 			$publicacionService = new PublicacionService;
-	
+
 			try {
-	
+
 				$this->publicacion = $publicacionService->getPublicacionById($this->id_publicacion);
-	
+
 			}catch(Exeption $e){
 				$logger = Logger::getRootLogger();
 				$logger->error($e);
 				return null;
 			}
 		}
-	
+		$this->nombre_publicacion = $this->publicacion->nombre; 
 		return $this->publicacion;
 	}
-	
+
 	//FUNCION QUE OBTIENE EL PATH DE ARCHIVOS DE UN NUMERO.
-	
+
 	public function getPath(){
 		$numeroService = new NumeroService;
 		$this->getPublicacion();
 		$pathname = $GLOBALS ['app_config'] ["ruta_publicaciones"] . $this->publicacion->id . "_" . $this->publicacion->nombre . "/numero" . $this->numero_revista."/";
-	
+
 		return $pathname;
 	}
 }
