d38426433737b7cd7b537fd69b07e3f22a9ca7c6
Tbots <matias.tavera@gmail.com>
2016-07-12 00:50:43 -0300

    cambio de articulo url_contenido a 400 varchar

--- editorialjr/helpers/ArticuloAjaxHelper.php
+++ editorialjr/helpers/ArticuloAjaxHelper.php
@@ -1,6 +1,10 @@
 <?php
 require_once(__DIR__."/../service/ArticuloService.php");
 require_once (__DIR__ . "/../model/ArticuloModel.php");
+require_once (__DIR__ . "/../service/NumeroService.php");
+require_once (__DIR__ . "/../model/NumeroModel.php");
+
+$numeroService = new NumeroService;
 
 $metodo = $_POST["metodo"];
 
@@ -25,17 +29,6 @@ switch($metodo){
 		$copete = $_POST['copete'];
 		$contenido_adicional = $_POST['contenido'];
 		$id_articulo = $_POST['id_articulo'];
-		/* imagen */
-
-		echo ("el titulo es ".$titulo);
-
-		if (file_exists($_FILES["imagen-file"])){
-			echo "existe la imagen";
-		} else {
-			echo "no existe";
-		}
-
-		/* guardo ?*/
 		$id_seccion = $_POST['seccion'];
 		$lat = $_POST['lat'];
 		$lng = $_POST['lng'];
@@ -45,6 +38,44 @@ switch($metodo){
 		$id_estado_articulo = 1;
 		$fecha_cierre = null;
 		$url_contenido = "http://www.rischiocalcolato.it/wp-content/uploads/2013/05/Default.jpg";
+
+		/* -----------------------------imagen-------------------------------- */
+
+
+		$numeroModel = $numeroService->getNumeroById($idNumero);
+		/*
+        //El nombre original del fichero en la máquina del cliente.
+        $_FILES['fichero_usuario']['name'];
+        //El nombre temporal del fichero en el cual se almacena el fichero subido en el servidor.
+        $_FILES['fichero_usuario']['tmp_name'];
+        //El código de error asociado a esta subida.
+        $_FILES['fichero_usuario']['error'];
+        */
+
+		$path = $numeroModel->getPath();
+
+		$fichero_subido = $GLOBALS['app_config']["ruta_publicaciones"] . $path . basename($_FILES['fichero_usuario']['name']);
+		$numeroModel->url_portada = $path . basename($_FILES['fichero_usuario']['name']);
+		$numeroModel->precio = $precio;
+		$numeroService->updateNumero($numeroModel);
+		//echo $fichero_subido;
+		if($_FILES['fichero_usuario']['size'] == 0) {
+			$id_publicacion = $numeroModel->id_publicacion;
+			$path = 'location:/../admin-listar-numeros.php?idpub='.$id_publicacion;
+			header($path);
+		}
+
+		if (move_uploaded_file($_FILES['fichero_usuario']['tmp_name'], $fichero_subido)) {
+			$id_publicacion = $numeroModel->id_publicacion;
+			$path = 'location:/../admin-listar-numeros.php?idpub='.$id_publicacion;
+			header($path);
+		} else {
+			echo "¡Posible ataque de subida de ficheros!\n";
+			echo 'Más información de depuración:';
+			print_r($_FILES);
+		}
+
+		/*--------------------------------------------------------------------------------*/
 
 		$result = $articuloService->createArticuloParametros($id_seccion, $id_user, $id_estado_articulo, $titulo, $lat, $lng, $fecha_cierre, $copete, $url_contenido, $contenido_adicional, $id_numero );
 		break;
--- editorialjr/js/EditarArticulo.js
+++ editorialjr/js/EditarArticulo.js
@@ -9,35 +9,8 @@ function editarArticulo(id_numero,id_articulo,id_user){
 		$("#idUser").val(id_user);
 		$("#contenido").val($('#summernote').summernote('code'));
 
-		/*var fm = $('#myForm');
-
-		var formdata = new FormData(fm[0]);
-
-		for (var key of formdata.keys()){
-			alert(key+" "+formdata.get(key));
-		}*/
 		
 		$('#myForm').submit();
 	});
 
-	function formEnviar(formdata, fm){
-		alert("entro");
-
-		$.ajax({
-			url         : '/helpers/ArticuloAjaxHelper.php',
-			data        : formdata ? formdata : fm.serialize(),
-			cache       : false,
-			contentType : false,
-			processData : false,
-			type        : 'POST',
-			success     : function(data, textStatus, jqXHR){
-				alert(data);
-				// Callback code
-			},
-			error       : function(error){
-				alert("error en el ajax!");
-			} 
-
-		});
-	}
 }
