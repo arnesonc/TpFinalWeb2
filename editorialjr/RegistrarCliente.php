add33efc0c5865da512102875b8baebabeadfd17
lucasnogueira955 <lucasnogueira955@outlook.com>
2016-07-12 21:36:06 -0300

    arreglos en la vista

--- editorialjr/RegistrarCliente.php
+++ editorialjr/RegistrarCliente.php
@@ -9,18 +9,72 @@ $arrayPaises = $paisService->getAllPais();
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<title>Registración</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css" />
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
-	<script src="../js/jquery-1.12.4.min.js" type="text/javascript"></script>
-	<script src="../js/common.js" type="text/javascript"></script>
-    <script src="js/jquery.redirect.js" type="text/javascript"></script>
-	<script src="../js/RegistrarCliente.js" type="text/javascript"></script>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Administración Editorial Jr</title>
+
+    <!-- Nuestro CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Side Bar CSS -->
+    <link href="css/clientStyle.css" rel="stylesheet">
+
+    <!-- CSS de las Datatables-->
+    <link rel="stylesheet" type="text/css" href="css/datatables.min.css" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
-<body>
-	<br>
+
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/index.php">Editorial Jr</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+    <div id="wrapper">
+<div class="container">
+  <div class="row">
+    <div id="wrapper">
 	<form name="frmFormulario" class="form-horizontal">
 		<fieldset>
 			<legend> Editorial Jr - Registración</legend>
@@ -142,5 +196,9 @@ $arrayPaises = $paisService->getAllPais();
 			</div>
 		</fieldset>
 	</form>
+	</div>
+	</div>
+	</div>
+	</div>
 </body>
 </html>
--- editorialjr/admin-login.php
+++ editorialjr/admin-login.php
@@ -3,25 +3,71 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>Administración Editorial Jr</title>
+    <title>Administración Editorial Jr</title>
 
-  <!-- Nuestro CSS -->
-  <link href="css/style.css" rel="stylesheet">
+    <!-- Nuestro CSS -->
+    <link href="css/style.css" rel="stylesheet">
 
-  <!-- Bootstrap Core CSS -->
-  <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Side Bar CSS -->
+    <link href="css/clientStyle.css" rel="stylesheet">
+
+    <!-- CSS de las Datatables-->
+    <link rel="stylesheet" type="text/css" href="css/datatables.min.css" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
-<body>
 
+    <body>
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/index.php">Editorial Jr</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+    <div id="wrapper">
+<div class="container">
+  <div class="row">
+    <div id="wrapper">
   <form id="signupForm" class="form-horizontal">
     <fieldset>
-      <legend> Editorial Jr - Inicio de sesión</legend>
+      <legend>Inicio de sesión</legend>
       <div class="form-group">
         <label class="col-md-4 control-label" for="email">Email</label>
         <div class="col-md-3">
@@ -52,7 +98,10 @@
       </div>
     </fieldset>
   </form><!-- /form -->
-
+</div>
+</div>
+</div>
+</div>
   <!-- Jquery-->
   <script src="js/jquery-1.12.4.min.js" type="text/javascript"></script>
 
